0f77c252d64153dc870c886d883427167a8cd69a
Divyarajsinh Sindhav <106440736+divyarajsinhsindhav@users.noreply.github.com>
2024-02-04 09:22:36 +0530

    Old Deleted

--- arrowFunc.js
+++
@@ -1,3 +0,0 @@
-const arrowSum = (a, b) => a+b
-
-console.log(arrowSum(5,2))
--- map.js
+++
@@ -1,8 +0,0 @@
-//.map() loops and returns the array
-const array = [1,2,3,4,5]
-array.map(numbers => {
-    console.log(numbers)
-}) 
-
-let result = array.map(numbers => numbers*2)
-console.log(result) 
