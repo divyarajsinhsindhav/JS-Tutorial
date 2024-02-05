130cff16b1d91b7df495e24fd74c60a3277202d6
Divyarajsinh Sindhav <106440736+divyarajsinhsindhav@users.noreply.github.com>
2024-02-05 23:10:51 +0530

    function and method

---
+++ functionAndMethod.js
@@ -0,0 +1,45 @@
+//Scope
+/*
+{
+  let x = 2;
+}
+// x can NOT be used here
+
+{
+  var x = 2;
+}
+// x CAN be used here
+
+for more details: https://www.w3schools.com/js/js_scope.asp
+*/
+
+const help = require("nodemon/lib/help");
+
+function sum(x,y) {
+    total = x + y;
+    return total;
+}
+
+console.log(sum(2,3));
+
+//Arrow Function
+/*
+It gets shorter! If the function has only one statement, and the statement returns a value, you can remove the brackets and the return keyword
+*/
+hello = () => "Hello World!";
+console.log(hello());
+
+multiplication = (a, b) => {
+    total = a * b;
+    return total;
+}
+console.log(multiplication(4,5));
+
+//method
+let array = [5,10,14,59];
+array.forEach(element => {
+    console.log(element);
+});
+
+//Map
+
