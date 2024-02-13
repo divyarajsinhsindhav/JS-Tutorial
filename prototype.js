843b29a7aa0a7b253e315dcee3928408a2deb226
Divyarajsinh Sindhav <106440736+divyarajsinhsindhav@users.noreply.github.com>
2024-02-13 08:43:27 +0530

    prototype

--- prototype.js
+++ prototype.js
@@ -14,3 +14,26 @@ const mainObj = {
 
 let user = mainObj.getDetails();
 console.log(user);
+
+
+
+const cities = ["Tokyo"];
+Array.prototype.show = function() {
+    return this;
+}
+
+console.log(cities.show())
+
+Array.prototype.convertIntoObject = function() {
+    let newObj = {};
+    this.forEach( element => {
+        newObj[element] = element;
+    })
+    return newObj;
+}
+
+console.log(cities.convertIntoObject());
+
+
+
+
